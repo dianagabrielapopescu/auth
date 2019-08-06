38c54fef054e30b601eb72884498f496eaa553bb
dianagabrielapopescu <diana.popescu000@gmail.com>
2019-08-06 10:08:11 +0300

    Update JwtConfig.java

--- src/main/java/org/jtmictoservices/auth/JwtConfig.java
+++ src/main/java/org/jtmictoservices/auth/JwtConfig.java
@@ -17,4 +17,44 @@ public class JwtConfig {
 
     @Value("${security.jwt.secret:JwtSecretKey}")
     private String secret;
+    
+        public String getUri() {
+        return Uri;
+    }
+
+    public void setUri(String uri) {
+        Uri = uri;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public int getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(int expiration) {
+        this.expiration = expiration;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
 }
